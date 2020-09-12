718d783ee27ada193a5062ee912cb740a60a7c31
RamnusKolsa <ramnus.kolsa@hotmail.com>
2020-09-12 18:42:05 +0300

    kova setti ajo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
+
+const Container = styled.div({
+  padding: 50
+})
+
+const FlexContainer = styled.div({
+  display: "flex",
+  justifyContent: "center"
+})
+
+const FlexItem = styled.div({
+  paddingRight: 50,
+
+})
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <FlexContainer>
+        <FlexItem>
+          Item 1
+        </FlexItem>
+        <FlexItem>
+          Item 2
+        </FlexItem>
+        <FlexItem>
+          Item 3
+        </FlexItem>
+      </FlexContainer>
+    </Container>
   );
 }
 
